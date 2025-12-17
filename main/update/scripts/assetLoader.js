8ab0a949ce0a0ad1ca162b457ca72f465d43ddba
ixlwork <168106522+ixlwork@users.noreply.github.com>
2025-12-17 10:23:51 -0600

    Create assetLoader.js

---
+++ main/update/scripts/assetLoader.js
@@ -0,0 +1,84 @@
+
+	export class AssetLoader{
+	constructor(baseUrl){
+	this._baseUrl = baseUrl || "";
+  this._loadingProgress = 0;
+  this._loadingTotal = 0;
+  this._loadingDone = 0;
+  this._isLoading = false;
+  this._loadingFail = false;
+  this._sources;
+  this._assets = [];
+	}
+	async loadLoadingScreen(){
+    try{
+	var url = this._baseUrl + "/assets/loadingScreen.json"
+  var loadingScreenObject = JSON.parse(await fetch(url).text());
+  var globalProperties = {};
+  await loadingScreenObject.init(loadingScreenObject);
+ await waitUntilLoading();
+ loadingScreenObject.load(loadingScreenObject);
+await finishLoading();
+    }catch(e){
+      console.error(e);
+     return false;
+    }
+return true;
+}
+  
+
+	
+	async loadAssetList(){
+    
+	this._sources = JSON.parse(await fetch(this._baseUrl + "/sources/src.json").text());
+    this._loadingTotal = this._sources.assets.length;
+    
+	}
+  async loadAssets(){
+    this._isloading = true;
+    try{
+await this.loadAssetList();
+for(let i = 0;i<this._loadingTotal;i++){
+  this._assets[i] = await sources.loadAsset(sources.assets[i]);
+}
+    }catch(e){
+
+    }
+  }
+ async finishLoading(){
+return new Promise((resolve, reject) => {
+
+
+function checkCondition() {
+if (this._loadingProgress = 1) {
+resolve(); // Condition is true, resolve the Promise
+} else if (this._loadingFail) {
+reject(new Error('Timeout waiting for condition')); // Timeout reached
+} else {
+// Not true yet and no timeout, re-check after an interval
+setTimeout(checkCondition, 100);
+}
+}
+
+checkCondition(); // Initial check
+})
+ }
+waitUntilLoading() {
+return new Promise((resolve, reject) => {
+const startTime = Date.now();
+
+function checkCondition() {
+if (this._isLoading) {
+resolve(); // Condition is true, resolve the Promise
+} else if (this._loadingFail) {
+reject(new Error('Timeout waiting for condition')); // Timeout reached
+} else {
+// Not true yet and no timeout, re-check after an interval
+setTimeout(checkCondition, 75);
+}
+}
+
+checkCondition(); // Initial check
+})
+}
+	}
