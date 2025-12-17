2422d2d2335130d5ce9e131960b8d412eb107ce5
ixlwork <168106522+ixlwork@users.noreply.github.com>
2025-12-17 11:03:17 -0600

    Update main.js

--- main/update/scripts/main.js
+++ main/update/scripts/main.js
@@ -1,1 +1,16 @@
- import AssetLoader from "/scripts/assetLoader.js";
+import AssetLoader from "/scripts/classes.js";
+import Renderer from "/scripts/classes.js";
+async function main(){
+try{
+var gl = document.getElementById("default-canvas").getContext("webgl2");
+var assetLoader = new AssetLoader(null, gl);
+await assetLoader.loadLoadingScreen();
+await assetLoader.loadAssets();
+
+}catch(e){
+  console.error(e);
+}
+}
+
+     
+main();
